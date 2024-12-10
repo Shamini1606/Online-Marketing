6fee643699be213fdf8fbc4b1cadae49629f84e8
Shamini1606 <shaminisiva16@gmail.com>
2024-12-10 16:11:10 +0530

    first commit

---
+++ verdano/info.php
@@ -0,0 +1,229 @@
+<html>
+
+
+<head>
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<title>Verdano Store - Online Shopping</title>
+        <link rel="stylesheet" type="text/css" href="estyle.css">
+
+        <link href="https://cdn.materialdesignicons.com/3.6.95/css/materialdesignicons.min.css" media="all" rel="stylesheet" type="text/css" />
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<!------ Include the above in your HEAD tag ---------->
+
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+
+
+      	<link style="width: 100%;height: 100%" rel="tab icon" href="store_images/logoicon.png"/>
+    
+    </head>
+
+
+
+<style>
+
+
+
+body
+{
+	background-color: #b5afaf;
+	background-position: right top;
+	background-attachment: fixed;
+}
+
+.info
+{
+	background-color: #c94242;
+	background-image: url('info_images/background3.jpg');
+	background-size: 1400px 800px;
+	height: auto;
+	width; 400px;
+	font-size: 100px;
+	font-weight: bold;
+	text-align: center;
+	color: red;
+	border-radius:16px;
+	margin: auto;
+	padding: 240px;
+}
+
+img
+{
+	border-radius:16px;
+}
+
+pre
+{
+	font-family: monospace;
+  white-space: pre;
+  
+}
+</style>
+
+	<div class="info" style="display:none">
+		<a href="welcome.php" style="color:#edf40c;text-decoration: none;">Verdano Store</a>
+	</div>
+	<body>
+		<h1 align="center" style="font-size:50px;color:white">About us</h1>
+		<div class= "container" style="overflow: hidden; display: flex; justify-content:space-around;">
+			<center>
+				<p style="font: size 30px;"> Welcome to Verdano, the premier online marketplace that redefines shopping as an immersive and inspiring journey. At Verdano, we believe in the power of connections — between people, passions, and products. Our platform bridges the gap between talented sellers and conscious buyers, creating a dynamic community driven by shared interests and a love for quality.
+
+Our story began with a simple yet powerful vision: to cultivate an online space where every transaction is more than just a purchase – it's an experience. We handpick a curated selection of products across diverse categories, from fashion and electronics to home decor and artisanal creations. Each item on Verdano is a testament to exceptional craftsmanship, innovation, and style.
+
+But Verdano is more than just a marketplace; it's a celebration of individuality and entrepreneurship. Our sellers are creators, dreamers, and visionaries who pour their hearts into every product they offer. We empower them to showcase their stories and passions, building personal connections with buyers who share their values.
+
+Shopping on Verdano means discovering products that resonate with your lifestyle and values. It means supporting small businesses, artisans, and innovators who are the backbone of our marketplace. It means joining a community that values quality, authenticity, and the thrill of finding something truly unique.
+
+Our commitment to excellence extends beyond the products we offer. We provide a seamless and secure shopping experience, supported by a responsive customer service team ready to assist you at every step. Verdano is your virtual marketplace, your source of inspiration, and your hub for all things extraordinary.
+
+Join us on this exciting journey of exploration and connection. Together, we're shaping the future of online shopping – one remarkable experience at a time. Welcome to Verdano, where shopping comes to life.</p>
+			</center>
+				
+		</div>
+	
+	
+	
+	
+	
+	
+	
+			<!-- Footer Starts From Here   -->
+
+		<footer class="ct-footer">
+			<div class="container">
+				<ul class="ct-footer-list text-center-sm">
+					<li>
+		        		<h2 class="ct-footer-list-header">Learn More</h2>
+				        <ul>
+				        	<li>
+				            	<a href="info.php">About us</a>
+				        	</li>
+				        	
+				        </ul>
+		      		</li>		      		
+		      		<li>
+		       			<h2 class="ct-footer-list-header">Our Team</h2>
+				        <ul>
+				          	<li>
+				            	<a href="info.php">Developers</a>
+				          	</li>
+				          	
+				        </ul>
+		      		</li>
+		      		
+		      		<li>
+		        		<h2 class="ct-footer-list-header">Customer Care</h2>
+		        		<ul>
+		          			<li>
+		            			<a href="">Help Centre</a>
+		          			</li>
+		          			<li>
+					            <a href="" onclick="alert('You can add money in your wallet through coupons. Coupons are avaiable at general stores. Add money using coupon and add products you want to buy in cart. After that press check out button. Products will be deleivered at your door step')" >How to buy?</a>
+					        </li>
+					        <li>
+					        	<a href="" onclick="alert('You can buy coupons from General Stores around your house')" >How to get coupon?</a>
+					        </li>
+					        <li>
+					            <a href="" onclick="alert('At your given address the products will be delievered');" >Delivery method</a>
+					        </li>
+		        		</ul>
+		      		</li>
+				     
+				    <li>
+				        <h2 class="ct-footer-list-header"> <a href="login.php" >Verdano Store</h2>
+				        <ul>
+					        <li>
+					            <a href="info.php">About Us</a>
+					        </li>
+					        <li>
+					            <a href="" onclick="alert('We will not be responsible if you give incorrect address. Products will be deleivered once on user confirmation. If user fails to receive, company is not responsible for it.')" >Trems and Conditions</a>
+					        </li>
+					        <li>
+					            <a href="" onclick="alert('You can email us \n  test@gmail.com')" >Contact Us</a>
+					        </li>
+				        </ul>
+				      </li>
+		    	</ul>
+		    
+
+			    <div class="ct-footer-meta text-center-sm">
+				    <div class="row">
+				        <div class="col-sm-6 col-md-2">
+				        	<img class="footerlogo" alt="logo" src="store_images/logoicon.png">
+				        </div>
+				        <div class="col-sm-6 col-md-3">
+				          	<address>
+				            	<span>Verdano Store Co.<br></span>
+				            	Colombo
+				            	<span>Phone: <a href="tel:5555555555">(000) 000-000</a></span>
+				          	</address>
+				        </div>
+				        <div class="col-sm-6 col-md-2 ct-u-paddingTop10">
+				          	<a href="" target="_blank"><img alt="app store" src="https://www.solodev.com/assets/footer/appstore.png"></a>
+				        </div>
+				        <div class="col-sm-6 col-md-2 ct-u-paddingTop10">
+				          	<a href="" target="_blank"><img alt="google play store" src="https://www.solodev.com/assets/footer/androidstore.png"></a>
+				        </div>
+				        <div class="col-sm-6 col-md-3">
+					        <ul class="ct-socials list-unstyled list-inline">
+					            <li>
+					              	<a href="" target="_blank"><img alt="facebook" src="https://www.solodev.com/assets/footer/facebook-white.png"></a>
+					            </li>
+					            <li>
+					              	<a href="" target="_blank"><img alt="twitter" src="https://www.solodev.com/assets/footer/twitter-white.png"></a>
+					            </li>
+					            <li>
+					              	<a href="" target="_blank"><img alt="youtube" src="https://www.solodev.com/assets/footer/youtube-white.png"></a>
+					            </li>
+					            <li>
+					              	<a href="" target="_blank"><img alt="instagram" src="https://www.solodev.com/assets/footer/instagram-white.png"></a>
+					            </li>
+					            <li>
+					              	<a href="" target="_blank"><img alt="pinterest" src="https://www.solodev.com/assets/footer/pinterest-white.png"></a>
+					            </li>
+					        </ul>
+				        </div>
+				    </div>
+			    </div>
+		  	</div>
+		  
+		  	<div class="ct-footer-post">
+		    	<div class="container">
+		      		<div class="inner-left">
+		        		<ul>
+		          			<li>
+		            			<a href="">FAQ</a>
+		          			</li>
+		          			<li>
+		            			<a href="">News</a>
+		          			</li>
+		          			<li>
+		            			<a href="">Contact Us</a>
+		          			</li>
+		        		</ul>
+		      		</div>
+		      		
+		      		<div class="inner-right">
+		        		<p>Copyright © 2023 Verdano StoreCo.&nbsp;<a href="">Privacy Policy</a></p>
+		        		<p><a class="ct-u-motive-color" href="" target="_blank">Web Design</a> by --- <a href="" target="_blank">verdano.com</a></p>
+		      		</div>
+		    	</div>
+		  	</div>
+		</footer>
+
+	
+	
+	
+	</body>
+	
+	
+	
+	
+	
+</html>
